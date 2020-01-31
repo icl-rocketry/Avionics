904acb499e8819b21f95306aee41baabbde5d1ce
Kiran <kiran@dyn3201-223.wlan.ic.ac.uk>
2020-01-31 19:21:03 +0000

    added webserver

---
+++ Ricardo_firmware/Ricardo_firmware/src/webserver.cpp
@@ -0,0 +1,13 @@
+#include "webserver.h"
+
+WiFiServer server(80);
+
+String header;
+
+
+void setupWebserver(){
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    server.begin();
+}
---
+++ Ricardo_firmware/Ricardo_firmware/src/webserver.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include "WiFi.h"
+#include "config.h"
+
+
+void setupWebserver();
