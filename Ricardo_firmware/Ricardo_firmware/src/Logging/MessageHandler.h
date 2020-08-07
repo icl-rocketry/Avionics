b6306ea159d628511468acf0aa5a45935c266935
Kiran <kiran@Kirans-MacBook-Pro.local>
2020-08-07 16:22:52 +0100

    more placeholders

--- Ricardo_firmware/Ricardo_firmware/src/Logging/MessageHandler.h
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-
-
-// message type: INFO, ERROR
-// importance: 1-logged on main_log; 2-broadcast over serial, logged on error_log; 3-audible warn, logged in critical_log
-void message(uint8_t type, bool log, bool alarm, String message);
