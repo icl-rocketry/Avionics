08c00ad0cac91f8c59738f695358c2a54978e498
Kiran <kiran@MacBook-Pro.local>
2020-09-12 14:21:13 +0100

    changed system_status variable to 32bit

--- Ricardo_OS/Ricardo_OS/src/Logging/messages.cpp
+++ Ricardo_OS/Ricardo_OS/src/Logging/messages.cpp
@@ -2,20 +2,20 @@
 
 
 //global system status variable intialized at 0
-uint16_t system_status = 0;
+uint32_t system_status = 0;
 
-void new_message(uint16_t msg,String reason){
+void new_message(uint32_t msg,String reason){
 
     system_status = system_status + msg;
 
 
     //todo detect when in usb debug mode and print reason over serial. Maybe worth logging reason to sd card??
-    
+
 
 
 };
 
-void delete_message(uint16_t msg){
+void delete_message(uint32_t msg){
 
     system_status = system_status - msg;
 
