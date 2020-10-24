fa756e168195ca9ae8b140abd496b1a1b07b4c71
Pavol Margitfalvi <margitpav1@gmail.com>
2020-10-24 13:42:13 +0200

    Added a function to decide the availability of a function in a given state.
    Co-authored-by: de Silva, Kiran <kd619@ic.ac.uk>

--- Ricardo_OS/Ricardo_OS/src/Comms/commandHandler.cpp
+++ Ricardo_OS/Ricardo_OS/src/Comms/commandHandler.cpp
@@ -29,7 +29,8 @@ void CommandHandler::update() {
 };
 
 uint8_t* CommandHandler::handleCommand(Command command) {
-
+	uint8_t curr_stateID = _sm->get_currStateID();
+	
 	switch (command.type) {
 		case COMMANDS::Launch:
 			break;
@@ -98,9 +99,57 @@ uint8_t* CommandHandler::handleCommand(Command command) {
 		case COMMANDS::Fire_pyro_2:
             break;
 		default:
-		//invalid command issued
+			//invalid command issued
 			break;
 
+	}
+
+	bool CommandHandler::commandAvailable(Command command, State state) {
+		// Checks if a comand can be executed in a given state
+		case COMMANDS::Launch:
+		case COMMANDS::Enter_USBMode:
+			return state == state::PREFLIGHT;
+		case COMMANDS::Reset:
+			return state == state::RECOVERY;
+		case COMMANDS::Abort:
+			return state == state::COUNTDOWN || state == state::FLIGHT;
+		case COMMANDS::Zero_Sensors:
+		case COMMANDS::Detailed_All_Sensors:
+		case COMMANDS::Detailed_Accel:
+		case COMMANDS::Detailed_Gyro:
+		case COMMANDS::Detailed_Mag:
+		case COMMANDS::Detailed_Baro:
+		case COMMANDS::Detailed_GPS:
+		case COMMANDS::Detailed_Battery:
+		case COMMANDS::Detailed_Throttle:
+		case COMMANDS::Callibrate_Accel:
+		case COMMANDS::Callibrate_Mag:
+		case COMMANDS::Callibrate_Gyro:
+		case COMMANDS::Callibrate_Baro:
+			return state == state::GROUNDSTATION || state == state::PREFLIGHT || return state == state::USBMODE;
+		case COMMANDS::Stop_Logging:
+		return state == state::PREFLIGHT || return state == state::USBMODE;
+		case COMMANDS::Start_Logging:
+    		return true;
+		case COMMANDS::Play_Song:
+            return state == state::GROUNDSTATION || state == state::PREFLIGHT || state == state::USBMODE || state == state::RECOVERY;
+		case COMMANDS::Clear_Flash:
+    	case COMMANDS::Clear_SD:
+            return state == state::GROUNDSTATION || state == state::PREFLIGHT;
+		case COMMANDS::Print_Flash:
+		case COMMANDS::Print_Sd:
+		case COMMANDS::Estimator_Output:
+		case COMMANDS::Raw_Sensor_Output:
+		case COMMANDS::Enter_Groundstation:
+		case COMMANDS::Enter_Countdown:
+		case COMMANDS::Enter_Flight:
+		case COMMANDS::Enter_Recovery:
+		case COMMANDS::Set_Throttle:
+		case COMMANDS::Fire_pyro_1:
+		case COMMANDS::Fire_pyro_2:
+            return state == state::USBMODE;
+		default:
+			return false;
 	}
 
 };
