dc774ccad64b42f884fece8e102f1413fc6635b0
Kiran <kiran@MacBook-Pro.local>
2020-11-17 23:38:26 +0000

    uncommented ttl because it is easier

--- Ricardo_OS/Ricardo_OS/src/Comms/packets.h
+++ Ricardo_OS/Ricardo_OS/src/Comms/packets.h
@@ -46,7 +46,7 @@ public:
     uint8_t source = 0x00; // Source interface ID for the packet
     uint8_t destination = 0x00; // Destination interface ID for the packet
 
-    //uint8_t ttl = 10; //time to live - prevents infinte rediretion of packets
+    uint8_t ttl = 10; //time to live - prevents infinte rediretion of packets
 
 };
 
