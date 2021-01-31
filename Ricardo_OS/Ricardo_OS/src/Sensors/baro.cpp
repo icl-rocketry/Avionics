64fb34bfd2562fa4d0dfeb8ff8c4c2aaff6b6c08
RuairidhScott-Brown <18rscottbrown@gmail.com>
2021-01-31 21:04:56 +0000

    Baro stuff

--- Ricardo_OS/Ricardo_OS/src/Sensors/baro.cpp
+++ Ricardo_OS/Ricardo_OS/src/Sensors/baro.cpp
@@ -15,6 +15,7 @@ _raw_data(raw_data)
 
 void Baro::setup(){
     //some sort of gps intilization idk
+    reset();
     
 }
 
@@ -23,4 +24,48 @@ void Baro::update(){
     if (_spi != NULL){ //check for null pointer to rpevent undfiend behaviour
   
     };
+}
+
+void Baro::reset() {
+    write8(BARO_CMD::MS5607_RESET);
+}
+
+void Baro::print_calibration() {
+    Serial.print("C1: ");
+    Serial.println(calibration.pressure_offset);
+    Serial.print("C2: ");
+    Serial.println(calibration.pressure_sensitivity);
+    Serial.print("C3: ");
+    Serial.println(calibration.temp_coef_pressure_sensitivity);
+    Serial.print("C4: ");
+    Serial.println(calibration.temp_coef_pressure_offset);
+    Serial.print("C5: ");
+    Serial.println(calibration.ref_temp);
+    Serial.print("C6: ");
+    Serial.println(calibration.temp_coef_temp);
+}
+
+
+void Baro::readProm() {
+    calibration.pressure_offset = read16(BARO_CMD::MS5607_PROM_C1);
+    calibration.pressure_sensitivity = read16(BARO_CMD::MS5607_PROM_C2);
+    calibration.temp_coef_pressure_sensitivity = read16(BARO_CMD::MS5607_PROM_C3);
+    calibration.temp_coef_pressure_offset = read16(BARO_CMD::MS5607_PROM_C4);
+    calibration.ref_temp = read16(BARO_CMD::MS5607_PROM_C5);
+    calibration.temp_coef_temp = read16(BARO_CMD::MS5607_PROM_C6);
+}
+
+uint16_t Baro::read16(const BARO_CMD command) {
+    uint16_t data {};
+    _spi->transfer(command);
+    data = _spi->transfer(0x00);
+    data <<= 8;
+    data |= _spi->transfer(0x00);
+    return data;
+}
+
+void Baro::write8(const BARO_CMD command){
+    digitalWrite(MS5607_CS, LOW);
+    _spi->transfer(command);
+    digitalWrite(MS5607_CS, HIGH);
 }
--- Ricardo_OS/Ricardo_OS/src/Sensors/baro.h
+++ Ricardo_OS/Ricardo_OS/src/Sensors/baro.h
@@ -7,6 +7,11 @@
 
 #include "sensorStructs.h"
 
+struct baro_data_t {
+    uint16_t temp;
+    uint16_t pressure;
+};
+
 class Baro{
     public:
         Baro(SPIClass* spi,SystemStatus* systemstatus,raw_measurements_t* raw_data);
@@ -20,6 +25,38 @@ class Baro{
         SystemStatus* _systemstatus;
         //pointer to raw data struct
         raw_measurements_t* _raw_data;
+
+        typedef struct {
+            uint16_t pressure_sensitivity {};            
+            uint16_t pressure_offset {};            
+            uint16_t temp_coef_pressure_sensitivity {};           
+            uint16_t temp_coef_pressure_offset {};           
+            uint16_t ref_temp {};            
+            uint16_t temp_coef_temp {};            
+        } BARO_CAL;
+
+        enum BARO_CMD {
+            MS5607_RESET                = 0x1E,
+            MS5607_CONVERT_D1           = 0x40,
+            MS5607_CONVERT_D2           = 0x50,
+            MS5607_ADC_READ             = 0x00,
+            MS5607_PROM_C1              = 0xA2,
+            MS5607_PROM_C2              = 0xA4,
+            MS5607_PROM_C3              = 0xA6,
+            MS5607_PROM_C4              = 0xA8,
+            MS5607_PROM_C5              = 0xAA,
+            MS5607_PROM_C6              = 0xAC
+            };
+        void reset();
+
+        void write8(BARO_CMD command);
+        
+        uint16_t read16(BARO_CMD command);
+
+        void readProm();
+
+
+
 
 
 };    
