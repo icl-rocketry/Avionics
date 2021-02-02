e2a3afdc1ee39a8b6251f503701333729cec5f0d
Kiran <kiran@macbook-pro-3.home>
2021-02-02 10:02:50 +0000

    added constructor to config controller'

--- Ricardo_OS/Ricardo_OS/src/Logging/configController.h
+++ Ricardo_OS/Ricardo_OS/src/Logging/configController.h
@@ -1,7 +1,13 @@
 #ifndef CONFIGCONTROLLER_H
 #define CONFIGCONTROLLER_H
 
+#include <string>
+
+
 class ConfigController{
+    public:
+        ConfigController(std::string filename);
+    private:
 
 };
 
