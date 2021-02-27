36142dfd8171d674c2bbd0b5ab892e2547807484
kiran <kd619@ic.ac.uk>
2021-02-27 10:48:58 +0000

    revised

--- Ricardo_OS/Ricardo_OS/src/Logging/itervar_improved.h
+++ Ricardo_OS/Ricardo_OS/src/Logging/itervar_improved.h
@@ -1,84 +1,6 @@
-#ifndef ITERVAR_H
-#define ITERVAR_H
-//template class for an iterable member variable type
-
-#include <string>
 #include <vector>
-#include "tostring.h"
-
-
-class iterVar_base{
-    public:     
-        virtual std::string stringify() = 0;
-        //virtual std::vector<uint8_t> serialize() = 0;
-        virtual void* get_ptr() = 0;
-        virtual void serialize(std::vector<uint8_t> &buf) = 0;
-        virtual void deserialize(const uint8_t* data) = 0;
-        virtual size_t type_size() = 0;
-};
-
-template<typename T>
-class iterVar:public iterVar_base{
-    public:
-        iterVar(T* var):
-        _ptr(var)
-        {};
-
-        std::string stringify(){
-            return tostring(*_ptr); //helper function to convert to string
-        }
-        /*
-        std::vector<uint8_t> serialize(){//serialization of variable
-            std::vector<uint8_t> bytearray(size);//intialize byte array
-            memcpy(bytearray.data(), _ptr,size);//copy bytes from variable to byte array
-            return bytearray;
-        }*/
-        void* get_ptr(){return _ptr;};
-        void serialize(std::vector<uint8_t> &buf){
-            //std::vector<uint8_t> bytearray = this->serialize();
-            uint8_t bytearray[size];
-            memcpy(bytearray,_ptr,size);
-            for (int i = 0; i<size;i++){
-                buf.push_back(bytearray[i]);
-            }
-        }
-        void deserialize(const uint8_t* data){
-            memcpy(_ptr,data,size);//copy size bytes from data buffer to pointer
-        }
-        size_t type_size(){
-            return size;
-        }
-    private:
-        T* _ptr;
-        size_t size = sizeof(T);
-
-};
-
-// F for your deleted code
-
-//example use
-/*
-class myclass{
-    public:
-        float a;
-        int b;
-        long c;
-
-        iterVar<float> a_iter{&a};
-        iterVar<int> b_iter{&b};
-        iterVar<long> c_iter{&c};
-
-        std::vector<iterVar_base*> member_variables{
-            &a_iter,
-            &b_iter,
-            &c_iter
-        };
-};
-*/
-#endif
-
-
-/*
+#include <tuple>
+#include <string>
 
 template <class C, class T>
 class serialisableElement
@@ -153,7 +75,7 @@ public:
     }
 
 };
-
+/*
 class myclass {
 
     static constexpr auto getSerialiser()
@@ -179,5 +101,4 @@ public:
         getSerialiser().deserialise(ret, buffer);
         return ret;
     }
-};
-*/
+};*/
