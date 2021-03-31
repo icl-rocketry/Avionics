62df42307103084e11b1e7adcc1e5d1aaa40b009
kiran <kd619@ic.ac.uk>
2021-03-31 13:26:02 +0100

    updated python

--- Ricardo_OS/Python_Tools/packet_testing/ricardo_comm_tester.py
+++ Ricardo_OS/Python_Tools/packet_testing/ricardo_comm_tester.py
@@ -37,7 +37,7 @@ ser.read(ser.in_waiting)
 
 while True:
 	# Test send a comand packet
-	header = Header(2, 0, 2, 0, source=2, destination=0) # source=4 for USB and destination=0 for rocket
+	header = Header(2, 0, 2, 0, source=4, destination=0) # source=4 for USB and destination=0 for rocket
 	cmd_packet = Command(header, 50, 0) # 50 for detailed all
 	serialized_packet = cmd_packet.serialize()
 	#print(serialized_packet.hex())
