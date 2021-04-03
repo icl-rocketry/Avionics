7c03c447f287155b434f01d14509b2eb9401d326
kiran <kd619@ic.ac.uk>
2021-04-03 01:37:29 +0100

    fixed wierd characters preventing telemtry log frame from working

--- Ricardo_OS/Ricardo_OS/src/Logging/logController.cpp
+++ Ricardo_OS/Ricardo_OS/src/Logging/logController.cpp
@@ -37,6 +37,19 @@ void LogController::log(state_t &estimator_state,raw_measurements_t &raw_sensors
     if((millis()-telemetry_prev_log_time) > telemetry_log_frequency || force){
 
         telemetry_frame.rawGPSLong = raw_sensors.gps_long; //continue for all variables - we need to see if thers a better way to do this
+        telemetry_frame.rawGPSLat = raw_sensors.gps_lat;
+        telemetry_frame.rawGPSsat = raw_sensors.gps_sat;
+        telemetry_frame.rawIMUAx= raw_sensors.ax;
+        telemetry_frame.rawIMUAy= raw_sensors.ay;
+        telemetry_frame.rawIMUAz= raw_sensors.az;
+        telemetry_frame.rawIMUGx= raw_sensors.gx;
+        telemetry_frame.rawIMUGy= raw_sensors.gy;
+        telemetry_frame.rawIMUGz= raw_sensors.gz;
+        telemetry_frame.rawIMUMx= raw_sensors.mx;
+        telemetry_frame.rawIMUMy= raw_sensors.my;
+        telemetry_frame.rawIMUMz= raw_sensors.mz;
+        telemetry_frame.rawIMUTemp= raw_sensors.imu_temp;
+        telemetry_frame.rawTimestamp = millis();
 
         telemetry_log_buffer.push_back(telemetry_frame); // add frame to buffer
 
@@ -97,9 +110,9 @@ void LogController::update(){
     //could be made neater if using for loop however this would make readabilty worse
     
     if ((millis() - prev_write_time[(uint8_t)LOG_TYPE::TELEMETRY]) > write_frequency[(uint8_t)LOG_TYPE::TELEMETRY]){
-        //write_to_file(LOG_TYPE::TELEMETRY);
+        write_to_file(LOG_TYPE::TELEMETRY);
         //Serial.println(telemetry_log_buffer.size());
-        telemetry_log_buffer.clear();
+        ///telemetry_log_buffer.clear();
         prev_write_time[(uint8_t)LOG_TYPE::TELEMETRY] = millis(); // update previous time
     }
     
@@ -124,6 +137,28 @@ void LogController::write_to_file(LOG_TYPE log_type){
     switch(log_type){
         case LOG_TYPE::TELEMETRY:
         {
+
+            microsd_file_path = microsd_prefix + telemetry_log_filename;
+            //flash_file_path = flash_prefix + system_log_filename;
+
+            File microsd_file = _storagecontroller->open(microsd_file_path,STORAGE_DEVICE::MICROSD,(O_WRITE | O_CREAT | O_AT_END));
+
+            if (!microsd_file){
+                return; // file is invalid
+            }
+            
+            for (int i = 0; i< telemetry_log_buffer.size();i++){
+                //processing each frame individually so we dont accidentally use all of heap
+                std::string entry = telemetry_log_buffer[i].stringify();
+                
+                microsd_file.print(entry.c_str());
+                //_storagecontroller->write(flash_file_path,entry,STORAGE_DEVICE::FLASH);
+                
+            }
+            
+            microsd_file.close();
+
+            telemetry_log_buffer.clear(); //clear all log frames in buffer
 
             break;
         }
--- Ricardo_OS/Ricardo_OS/src/Logging/logframe.h
+++ Ricardo_OS/Ricardo_OS/src/Logging/logframe.h
@@ -12,111 +12,7 @@ class logframe{
     virtual std::string stringify() const; //purley virtaul seralize method
 };*/
 
-class telemetry_logframe{
-private:  
-    static constexpr auto getSerializer()
-    {
-        auto ret = serializer(
-            &telemetry_logframe::rawGPSLong,
-            &telemetry_logframe::rawGPSLong,
-            &telemetry_logframe::rawGPSLat,
-            &telemetry_logframe::rawGPSSpeed,
-            &telemetry_logframe::rawGPSAlt,
-            &telemetry_logframe::rawGPSsat,
-            &telemetry_logframe::rawGPShdop,
-            &telemetry_logframe::rawIMUAx,
-            &telemetry_logframe::rawIMUAy,
-            &telemetry_logframe::rawIMUAz,
-            &telemetry_logframe::rawIMUGx,
-            &telemetry_logframe::rawIMUGy,
-            &telemetry_logframe::rawIMUGz,
-            &telemetry_logframe::rawIMUMx,
-            &telemetry_logframe::rawIMUMy,
-            &telemetry_logframe::rawIMUMz,
-            &telemetry_logframe::rawIMUTemp,
-            &telemetry_logframe::rawBaroTemp,
-            &telemetry_logframe::rawBaroPres,
-            &telemetry_logframe::rawBaroAlt,
-            &telemetry_logframe::battVolt,
-            &telemetry_logframe::battPercent,
-            &telemetry_logframe::rawTimestamp,
-            &telemetry_logframe::estimatorX,
-            &telemetry_logframe::estimatorY,
-            &telemetry_logframe::estimatorZ,
-            &telemetry_logframe::estimatorVX,
-            &telemetry_logframe::estimatorVY,
-            &telemetry_logframe::estimatorVZ,
-            &telemetry_logframe::estimatorAX,
-            &telemetry_logframe::estimatorAY,
-            &telemetry_logframe::estimatorAZ,
-            &telemetry_logframe::estimatorYaw,
-            &telemetry_logframe::estimatorPitch,
-            &telemetry_logframe::estimatorRoll,
-            &telemetry_logframe::estimatorP,
-            &telemetry_logframe::estimatorQ,
-            &telemetry_logframe::estimatorR, 
-            &telemetry_logframe::estimatorL,
-            &telemetry_logframe::estimatorM,
-            &telemetry_logframe::estimatorN,
-            &telemetry_logframe::estimatorTimestamp
-            );
-        return ret;
-    }
-    
-    /*
-    static constexpr size_t packet_size() {
-        return getSerializer().member_size();
-    }*/
 
-public:   
-    double rawGPSLong; //gps.long
-    double rawGPSLat; //gps.lat
-    double rawGPSSpeed; //gps.speed
-    double rawGPSAlt; //gps.alt
-    uint8_t rawGPSsat; //gps.sat
-    double rawGPShdop; //gps.hdop
-    double rawIMUAx; //imu.ax
-    double rawIMUAy; //imu.ay
-    double rawIMUAz; //imu.az
-    double rawIMUGx; //imu.gx
-    double rawIMUGy; //imu.gy
-    double rawIMUGz; //imu.gz
-    double rawIMUMx; //imu.mx
-    double rawIMUMy; //imu.my
-    double rawIMUMz; //imu.mz
-    double rawIMUTemp; //imu.temp
-    double rawBaroTemp; //baro.temp
-    double rawBaroPres; //baro.pres
-    double rawBaroAlt; //baro.alt
-    int battVolt; //batt.volt
-    int battPercent; //batt.percent
-    long rawTimestamp; //raw.timestamp
-    double estimatorX; //estimator.x
-    double estimatorY; //estimator.y
-    double estimatorZ; //estimator.z
-    double estimatorVX; //estimator.vx
-    double estimatorVY; //estimator.vy
-    double estimatorVZ; //estimator.vz
-    double estimatorAX;  //estimator.ax
-    double estimatorAY; //
-    double estimatorAZ; //estimator.az
-    double estimatorYaw; //estimator.yaw
-    double estimatorPitch;
-    double estimatorRoll; //estimator.roll
-    double estimatorP; //estimator.p
-    double estimatorR;
-    double estimatorQ; //estimator.q
-    double estimatorL; //estimator.l
-    double estimatorM;
-    double estimatorN; //estimator.n
-    long estimatorTimestamp;
-
-
-    std::string stringify()const{
-        return getSerializer().stringify(*this) + "\n";
-    };
-
-};
 class system_logframe{
 private:  
     static constexpr auto getSerializer()
@@ -171,7 +67,54 @@ class engine_logframe{
  // todo
 };
 
+class telemetry_logframe{
+private:  
+    static constexpr auto getSerializer()
+    {
+        auto ret = serializer(
+            &telemetry_logframe::rawGPSLong,
+            &telemetry_logframe::rawGPSLat,
+            &telemetry_logframe::rawGPSsat,
+            &telemetry_logframe::rawIMUAx,
+            &telemetry_logframe::rawIMUAy,
+            &telemetry_logframe::rawIMUAz,
+            &telemetry_logframe::rawIMUGx,
+            &telemetry_logframe::rawIMUGy,
+            &telemetry_logframe::rawIMUGz,
+            &telemetry_logframe::rawIMUMx,
+            &telemetry_logframe::rawIMUMy,
+            &telemetry_logframe::rawIMUMz,
+            &telemetry_logframe::rawIMUTemp,
+            &telemetry_logframe::rawTimestamp
+            
+            
 
+        );
+        return ret;
+    }
+
+public:
+    //global_system_flag
+    long rawGPSLong;
+    long rawGPSLat;
+    int rawGPSsat;
+    float rawIMUAx;
+    float rawIMUAy;
+    float rawIMUAz;
+    float rawIMUGx;
+    float rawIMUGy;
+    float rawIMUGz;
+    float rawIMUMx;
+    float rawIMUMy;
+    float rawIMUMz;
+    uint16_t rawIMUTemp;
+    uint64_t rawTimestamp;
+
+    std::string stringify()const{
+        return getSerializer().stringify(*this) + "\n";
+    };
+
+};
 
 
 
