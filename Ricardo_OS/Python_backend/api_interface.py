f3864306c86a3e38333e48fcc96cb323e9f7a2d4
kiran <kd619@ic.ac.uk>
2021-04-08 02:05:31 +0100

    added socketio

--- Ricardo_OS/Python_backend/api_interface.py
+++ Ricardo_OS/Python_backend/api_interface.py
@@ -1,12 +1,39 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
-from flask_pymongo import PyMongo
+from flask import Flask,jsonify,request,Response
+from flask_socketio import SocketIO, emit
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+app.config['DEBUG'] = True
 
-app.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONDO_URI'] = 'mongodb://localhost:27017/restdb'
+socketio = SocketIO(app)
 
-mongo = PyMongo(app)
+@app.route('/')
+def index():
+    return "RICARDO OS PYTHON BACKEND",200
 
+@app.route('/command', methods=['POST'])
+def send_command():
+    command_data = request.json
+    return 'OK',200
+
+@socketio.on('connect')
+def connect();
+    emit('Connect',{'data':'Connected'})
+
+@socketio.on('disconnect')
+def disconnect():
+    print('client disconnected')
+    
+@socketio.on('get_telemetry')
+def getTelemetry(json):#retrieve data from db or return error if no rocket connected
+    telemetryData = jsonify(
+        data='telemetry'
+    )
+    return telemetryData,200
+
+
+
+
+if __name == '__main__':
+    socketio.run(app,port=5000)
