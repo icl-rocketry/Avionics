d560ed90810b582533482a9f1bbbbf76de73f7da
Samuel Wang <samuelpswang@gmail.com>
2021-11-20 13:33:21 +0000

    Refactor: Moved fronend directory location

--- Ricardo_OS/Python_backend/frontend/src/App.js
+++ Ricardo_OS/Python_backend/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Richardo Backend Placeholder
+          RICARDO Backend Placeholder
         </p>
       </header>
     </div>
